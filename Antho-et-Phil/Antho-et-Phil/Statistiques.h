142f12cb61246f1792752e60bfb97537ee0521b1
Anthony Ramsay <Anthorapido@gmail.com>
2025-01-25 13:51:27 -0500

    Ajout de certaine définition dans le .h (à révisé)

--- Antho-et-Phil/Antho-et-Phil/Statistiques.h
+++ Antho-et-Phil/Antho-et-Phil/Statistiques.h
@@ -5,5 +5,14 @@
 using namespace std;
 class Statistiques
 {
+private:
+	
+public:
+	double Moyenne(double[]);
+	double Mediane(double[]);
+	double EcartType(double[]);
+
+	double DistanceEucli(double[]);
+	double DistanceMan(double[]);
 };	
 
